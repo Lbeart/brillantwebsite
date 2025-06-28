ceab5b17b30f62b676351d4cccb61f4dbcf37f0f
Leart Bytyqi <leart.bytyqii98@gmail.com>
2025-06-29 00:54:11 +0200

    ✅ Fixed APP_URL and home redirect

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
+# Set permissions again in case container reset them
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
-mkdir -p storage/logs bootstrap/cache
-chown -R www-data:www-data storage bootstrap/cache
-chmod -R 775 storage bootstrap/cache
-
+# Laravel optimizations
 php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 php artisan migrate --force || true
 
+# Start Supervisor to keep services running
 /usr/bin/supervisord -c /etc/supervisord.conf
