154e596f1f94888b2a0ceee6a2645ffdfe304b1f
Leart Bytyqi <leart.bytyqii98@gmail.com>
2025-06-29 00:23:04 +0200

    Ndryshuar APP_URL dhe konfigurimet për domain-in b-brillant.com

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+   'url' => env('APP_URL', 'https://b-brillant.com'),
 
     /*
     |--------------------------------------------------------------------------
