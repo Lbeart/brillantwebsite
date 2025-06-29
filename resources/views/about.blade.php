5ef454b0f3e8ff24d6fb07352181958222fb6fed
Leart Bytyqi <leart.bytyqii98@gmail.com>
2025-06-29 02:29:17 +0200

    Përditësim i konfigurimit për lidhjen me databazën në Railway

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,6 +6,7 @@
   <title>Brillant - About Us</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="icon" type="image/png" href="{{ asset('images/llogo.png') }}">
   <style>
     body { font-family: 'Poppins', sans-serif;
             background-color: #f7f9fc;
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -7,6 +7,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="icon" type="image/png" href="{{ asset('images/llogo.png') }}">
   <style>
     body { margin: 0; padding: 0; }
     /* Wrapper to position navbar over slider */
